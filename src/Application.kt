c4c3805a3bd9bedbe8107cb5cd26b6c529d8cdb8
orangain <orangain@gmail.com>
2019-09-22 14:30:47 +0900

    Add routing

--- src/Application.kt
+++ src/Application.kt
@@ -3,11 +3,18 @@ package com.example
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get // 追加
+import io.ktor.routing.routing // 追加
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    routing { // 追加
+        get("/") {
+            call.respondText("Hello, Ktor!")
+        }
+    }
 }
 
